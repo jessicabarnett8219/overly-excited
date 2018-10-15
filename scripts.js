abab10efd763a715a872fe7735634e434c53bd73
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-15 18:51:04 -0500

    refactored function to accept different symbols as an argument.

--- scripts.js
+++ scripts.js
@@ -2,15 +2,15 @@
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
 
-function addExcitement (theWordArray) {
+function addExcitement (theWordArray, symbol) {
     let buildMeUp = ""
     for (let i = 0; i < theWordArray.length; i++) {
       if ((i+1) % 3 === 0) {
-        buildMeUp += theWordArray[i] + "! ";
+        buildMeUp += theWordArray[i] + symbol + " ";
       } else {
         buildMeUp += theWordArray[i] + " "     
     } console.log(buildMeUp)
   }
   
 }
-addExcitement(sentence)
+addExcitement(sentence, "?")
